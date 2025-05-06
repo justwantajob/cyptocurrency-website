83277ec9294cc12856024f56e37a1e6e33d26c8c
gurur <kisaoglugurur@gmail.com>
2025-05-06 06:34:09 +0300

    Fetch coin data and show them on the page

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,45 @@
-console.log('App started.');
+"use strict";
+
+
+async function fetchCryptoData() {
+  let cryptoInfoHTML = '';
+  // const cryptoInfoList = document.querySelector('.js-cryptocurrency-info-list');
+  const cryptoCurrencyInfoTableBody = document.querySelector('.js-cryptocurrency-info-table-body');
+
+  try {
+    const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd');
+    const cryptoCurrencyData = await response.json();
+
+    cryptoCurrencyData.slice(0, 10).forEach((coin) => {
+      const formattedPrice = coin.current_price.toLocaleString(
+        'en-US',
+        {
+          style: 'currency',
+          currency: 'USD',
+        }
+      );
+      // cryptoInfoHTML += `
+      //     <li>${coin.name} | $${coin.current_price.toLocaleString()} | <img style="height:16px;" src="${coin.image}"></li>
+      // `;
+
+      const cryptocurrencyTableRow = document.createElement('tr');
+      cryptocurrencyTableRow.innerHTML += `
+        <td>${coin.name}</td>
+        <td>${formattedPrice}</td>
+        <td><img style="height:16px;" src="${coin.image}"></td>
+      `;
+
+      cryptoCurrencyInfoTableBody.appendChild(cryptocurrencyTableRow);
+
+    });
+  } catch (error) {
+    console.log('Error fetching crypto data: ', error);
+  }
+
+  // cryptoInfoList.innerHTML = cryptoInfoHTML;
+}
+
+fetchCryptoData();
+
+// Auto refresh feature
+// setInterval(fetchCryptoData, 60000);
